da06d5bf5491f313f8ae660a28b458b7168f5a2c
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-03 19:08:18 -0300

    Added auto startx

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -8,4 +8,9 @@ export LANG=C
 set scr_dir $HOME/.scripts
 
 echo $PATH | grep $scr_dir > /dev/null || set -g PATH "$PATH:$scr_dir"
+
 alias g='/usr/bin/git --git-dir=/home/mateus/.cfg/ --work-tree=/home/mateus'
+
+if [ (tty) = "/dev/tty1" ];
+	pgrep i3 || startx
+end
