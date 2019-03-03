740a523dc8cccc83be5b1593996facc5e0074b5d
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-03 16:34:43 -0300

    Fix path issue

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,4 +1,5 @@
-set TERMINAL "rxvt-unicode"
+set -g TERMINAL "rxvt-unicode"
+set -g BROWSER "google-chrome-stable"
 
 sudo loadkeys br-abnt2
 
@@ -6,5 +7,5 @@ export LANG=C
 
 set scr_dir $HOME/.scripts
 
-echo $PATH | grep $scr_dir > /dev/null || set PATH "$PATH $HOME/.scripts"
+echo $PATH | grep $scr_dir > /dev/null || set -g PATH "$PATH:$scr_dir"
 alias g='/usr/bin/git --git-dir=/home/mateus/.cfg/ --work-tree=/home/mateus'
