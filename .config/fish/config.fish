b180f8f5e38e3322936821ccb8bff8f5a3a59c0e
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-03 22:51:09 -0300

    Removed ugly language hack

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,10 +1,6 @@
 set TERMINAL "rxvt-unicode"
 set BROWSER "google-chrome-stable"
 
-sudo loadkeys br-abnt2
-
-export LANG=C
-
 set scr_dir $HOME/.scripts
 
 echo $PATH | grep $scr_dir > /dev/null || set PATH "$PATH:$scr_dir"
