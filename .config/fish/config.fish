15127069494489f55f68b8bd4a263da5ed546e58
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-03 21:28:49 -0300

    Changed hardcoded home dir to $HOME on fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -9,7 +9,7 @@ set scr_dir $HOME/.scripts
 
 echo $PATH | grep $scr_dir > /dev/null || set PATH "$PATH:$scr_dir"
 
-alias g='/usr/bin/git --git-dir=/home/mateus/.cfg/ --work-tree=/home/mateus'
+alias g='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
 if [ (tty) = "/dev/tty1" ];
 	pgrep i3 || startx
