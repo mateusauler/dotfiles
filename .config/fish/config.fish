e53f516e8590540080368c3ea3cea1c24d379b44
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-05 15:40:41 -0300

    Turn off speaker beep fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,6 +7,8 @@ echo $PATH | grep $scr_dir > /dev/null || set PATH "$PATH:$scr_dir"
 
 alias g='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
+sudo rmmod pcspkr
+
 if [ (tty) = "/dev/tty1" ];
 	pgrep i3 || startx
 end
