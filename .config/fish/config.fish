427a6985851105e5b6043777209fd97d40cb470c
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-05 16:33:09 -0300

    Only remove pcspkr module if it's not loaded

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,9 @@ echo $PATH | grep $scr_dir > /dev/null || set PATH "$PATH:$scr_dir"
 
 alias g='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-sudo rmmod pcspkr
+if [ (lsmod | grep pcspkr) != "" ];
+	sudo rmmod pcspkr
+end
 
 if [ (tty) = "/dev/tty1" ];
 	pgrep i3 || startx
