2c9d1d7a7c781bfe292603b77dbf8b795c5810ac
Mateus Auler <mateusaulerlima@gmail.com>
2019-03-05 18:14:01 -0300

    Changed disabling of pcspkr

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,9 +7,7 @@ echo $PATH | grep $scr_dir > /dev/null || set PATH "$PATH:$scr_dir"
 
 alias g='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
 
-if [ (lsmod | grep pcspkr) != "" ];
-	sudo rmmod pcspkr
-end
+lsmod | grep pcspkr > /dev/null && sudo rmmod pcspkr
 
 if [ (tty) = "/dev/tty1" ];
 	pgrep i3 || startx
