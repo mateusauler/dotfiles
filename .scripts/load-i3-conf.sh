51255e1d99d93746b1417c28c1728274bba6d45c
Mateus Auler <mateusaulerlima@gmail.com>
2019-08-09 17:57:39 -0300

    Dirty fix i3-gaps restarting bug

--- .scripts/load-i3-conf.sh
+++ .scripts/load-i3-conf.sh
@@ -2,10 +2,10 @@
 
 # Create a symbolic link to the desired config file and call it config-ln
 
-cat $HOME/.config/i3/config-base > $HOME/.config/i3/config
+cp $HOME/.config/i3/config-base $HOME/.config/i3/config
 
 if [ -f $HOME/.config/i3/config-ln ]; then
-
 	cat $HOME/.config/i3/config-ln >> $HOME/.config/i3/config
-
 fi
+
+i3-msg restart
