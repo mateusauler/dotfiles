75b6206e236c68d629bccc44f8cab9b9cfdd6713
Mateus Auler <mateusauler@protonmail.com>
2019-10-18 17:01:37 -0300

    Added my keyboard layout indicator

--- .scripts/toggle_lang.sh
+++ .scripts/toggle_lang.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-if [ $(setxkbmap -query | grep layout | awk '{print $2}')  = "us" ]; then
-	layout=br
+if [ $# -eq "1" ]; then
+	layout=$1
 else
-	layout=us
+	if [ $(setxkbmap -query | grep layout | awk '{print $2}')  = "us" ]; then
+		layout=br
+	else
+		layout=us
+	fi
 fi
 
 setxkbmap -layout $layout
