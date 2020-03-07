bb88e7a8f5b49eb4b708b390a11196869508808e
Mateus Auler <mateusauler@protonmail.com>
2020-03-07 20:03:27 -0300

    Removed fish greeting and aliased hexdump

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,8 @@
 set TERMINAL "rxvt-unicode"
 set BROWSER "firefox"
 
+set fish_greeting
+
 set short_dir $HOME/.shortcuts
 
 echo $PATH | grep $short_dir > /dev/null || set PATH "$PATH:$short_dir"
@@ -11,6 +13,8 @@ lsmod | grep pcspkr > /dev/null && sudo rmmod pcspkr
 
 abbr --add p pushd
 abbr --add o popd
+
+alias hexdump="hexdump -C"
 
 if [ (tty) = "/dev/tty1" ];
 	pgrep i3 || startx
