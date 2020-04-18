6da80894b88083b43a21a484b88341563f82a97e
Mateus Auler <mateusauler@protonmail.com>
2020-04-18 05:37:43 -0300

    Fish shows current vi mode on the left prompt

--- .config/fish/functions/fish_prompt.fish
+++ .config/fish/functions/fish_prompt.fish
@@ -22,10 +22,34 @@ function fish_prompt --description 'Write out the prompt'
 	set -l color_bg_pwd '#6393ed'
 	set -l color_bg_git '#76bf01'
 	set -l color_fg_git $color_fg_pwd
+	set -l color_bg_vip '#4d4'
+	set -l color_fg_vip '#000'
+
+	set -l curr_mode
+
+    switch $fish_bind_mode
+        case default
+            set curr_mode 'N'
+        case insert
+            set curr_mode 'I'
+        case replace_one
+            set curr_mode 'R1'
+        case replace
+            set curr_mode 'R'
+        case visual
+            set curr_mode 'V'
+    end
+
+#	set curr_mode '[' $curr_mode ']'
+
 
 	if tty | grep '/dev/tty' > /dev/null
-		echo -n -s $USER ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal)  ' ' $suffix ' '
+		echo -n -s $curr_mode ' ' $USER ' ' (set_color $color_cwd) (prompt_pwd) (set_color normal)  ' ' $suffix ' '
 	else
+		echo -n -s (set_color -b $color_bg_vip) (set_color $color_fg_vip) " $curr_mode " \
+				   (set_color -b $color_bg_usr) (set_color $color_bg_vip) '' \
+				   (set_color normal)
+
 	    echo -n -s (set_color -b $color_bg_usr) " $USER " \
 				   (set_color -b $color_bg_pwd) (set_color $color_bg_usr) ' ' \
 				   (set_color normal) (set_color -b $color_bg_pwd) (set_color $color_fg_pwd) (prompt_pwd) ' ' \
