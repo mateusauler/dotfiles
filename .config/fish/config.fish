0356c855a4c3dda18ee9dee5e541e65d0f7288fc
Mateus Auler <mateusauler@protonmail.com>
2020-04-25 18:22:39 -0300

    Fish config exporting env variables, terminal is now alacritty, removes the beeps when starting X11

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,15 +1,13 @@
-set TERMINAL "rxvt-unicode"
-set BROWSER "firefox-developer-edition"
+export TERMINAL="alacritty"
+export BROWSER="firefox-developer-edition"
 
 set fish_greeting
 
-set short_dir $HOME/.shortcuts
+set -l short_dir $HOME/.shortcuts
 
 echo $PATH | grep $short_dir > /dev/null || set PATH "$PATH:$short_dir"
 
 alias g='/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME'
-
-lsmod | grep pcspkr > /dev/null && sudo rmmod pcspkr
 
 abbr --add p pushd
 abbr --add o popd
@@ -22,5 +20,6 @@ fish_vi_key_bindings
 
 if test (tty) = "/dev/tty1" && ! pgrep i3;
 	test -f $HOME/.scripts/mount_devices.sh && $HOME/.scripts/mount_devices.sh &
+	lsmod | grep pcspkr > /dev/null && sudo rmmod pcspkr &
 	startx
 end
