158583716791490bb4aac2550b7410811a9c496c
Mateus Auler <mateusauler@protonmail.com>
2020-07-16 05:26:09 -0300

    Changes to config.fish:
    - Changed export to set -x
    - Added XDG directory variables
    - Added ~/ cleanup variables and aliases
    - Removed the '=' from the alias declarations
    - Added a couple of aliases

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,12 +1,51 @@
-export TERM="st-256color"
-export TERMINAL=$TERM
+# environment
 
-export EDITOR="vim"
+set -x TERM		st-256color
+set -x TERMINAL	$TERM
 
-export BROWSER="firefox-developer-edition"
-export BROWSER_PRIV="$BROWSER --private-window"
+set -x EDITOR vim
 
-export WM="dwm"
+set -x BROWSER		firefox-developer-edition
+set -x BROWSER_PRIV "$BROWSER --private-window"
+
+set -x WM dwm
+
+# XDG variables
+
+set -x XDG_CONFIG_HOME	$HOME/.config
+set -x XDG_CACHE_HOME	$HOME/.cache
+set -x XDG_DATA_HOME	$HOME/.local/share
+
+set -x XDG_DATA_DIRS	/usr/local/share:/usr/share
+set -x XDG_CONFIG_DIRS	/etc/xdg
+
+set -x XDG_DESKTOP_DIR		-
+set -x XDG_DOWNLOAD_DIR		$HOME/dl
+set -x XDG_TEMPLATES_DIR	$HOME/templates
+set -x XDG_PUBLICSHARE_DIR	$HOME/pub
+set -x XDG_DOCUMENTS_DIR	$HOME/docs
+set -x XDG_MUSIC_DIR		$HOME/music
+set -x XDG_PICTURES_DIR		$HOME/pics
+set -x XDG_VIDEOS_DIR		$HOME/vids
+
+# cleanup ~/
+
+set -x XAUTHORITY		$XDG_RUNTIME_DIR/Xauthority
+set -x GTK2_RC_FILES	$XDG_CONFIG_HOME/gtk-2.0/gtkrc-2.0
+set -x LESSHISTFILE		-
+set -x WGETRC			$XDG_CONFIG_HOME/wget/wgetrc
+set -x GNUPGHOME		$XDG_DATA_HOME/gnupg
+set -x WINEPREFIX		$XDG_DATA_HOME/wineprefixes/default
+set -x GOPATH			$XDG_DATA_HOME/go
+set -x RXVT_SOCKET      $XDG_RUNTIME_DIR/urxvtd
+set -x SQLITE_HISTORY	$XDG_DATA_HOME/sqlite_history
+set -x VIMINIT			'source $XDG_CONFIG_HOME/vim/vimrc'
+
+alias nvidia-settings	"nvidia-settings --config=$XDG_CONFIG_HOME/nvidia/settings"
+alias wget				"wget --hsts-file $XDG_CACHE_HOME/wget-hsts"
+alias sqlite3			"sqlite3 -init $XDG_CONFIG_HOME/sqlite3/sqliterc"
+
+# initialization
 
 set fish_greeting
 
@@ -14,10 +53,13 @@ set -l short_dir $HOME/.shortcuts
 
 echo $PATH | grep $short_dir > /dev/null || set PATH "$PATH:$short_dir"
 
-alias g=git
-alias gua="git remote | xargs -L1 git push --all"
-alias gsu="git remote | xargs -L1 -I {} git push -u {} (git branch --show-current)"
-alias cua="c remote | xargs -L1 c push --all"
+# aliases & abbreviations
+
+alias g		git
+alias gua	"git remote | xargs -L1 git push --all"
+alias gsu	"git remote | xargs -L1 -I {} git push -u {} (git branch --show-current)"
+alias c		"/usr/bin/git --git-dir=$HOME/.cfg/ --work-tree=$HOME"
+alias cua	"c remote | xargs -L1 c push --all"
 
 alias cleanup-after-bad-patch="rm *.{rej, orig}"
 
@@ -38,8 +80,7 @@ abbr --add ch "c checkout"
 
 abbr --add pa "patch -p1 <"
 
-abbr --add p pushd
-abbr --add o popd
+abbr --add p "sudo pacman -S"
 
 alias hexdump="hexdump -C"
 
@@ -48,6 +89,8 @@ alias la="exa -laGF --group-directories-first"
 alias ll="exa -lGF --group-directories-first"
 
 fish_vi_key_bindings
+
+# startx if in tty1
 
 if test (tty) = "/dev/tty1" && ! pgrep $WM;
 	test -f $HOME/.scripts/mount_devices.sh && $HOME/.scripts/mount_devices.sh &
