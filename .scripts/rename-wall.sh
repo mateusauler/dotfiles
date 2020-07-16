904e6c73d57bb9b6694686cc27b4a1681826a61a
Mateus Auler <mateusauler@protonmail.com>
2020-07-16 05:23:14 -0300

    Wallpaper rename script trims the hash to 16 characters instead of 40

--- .scripts/rename-wall.sh
+++ .scripts/rename-wall.sh
@@ -5,7 +5,7 @@ cd $XDG_PICTURES_DIR/wall
 ls | while read file; do
 	ext="${file##*.}"
 
-	newname=$(sha1sum $(pwd)/$file | cut -b -40).$ext
+	newname=$(sha1sum $(pwd)/$file | cut -b -16).$ext
 	
 	if [ ! -f $newname ]; then
 		mv $file $newname
