72f1b7835d817bc5680ed431d81494c336a076be
Mateus Auler <mateusauler@protonmail.com>
2020-08-13 13:39:55 -0300

    add shortcut script has option to remove shortcut

--- .scripts/add_shortcut.sh
+++ .scripts/add_shortcut.sh
@@ -4,11 +4,17 @@ scr_dir=$HOME/.shortcuts
 
 if [ "$#" = 2 ]; then
 	if [ -e "$scr_dir/$2" ]; then
-		echo "Shortcut already exists."
+		if [ "$1" = "-r" ]; then 
+			rm $scr_dir/$2
+		else
+			echo "Shortcut already exists."
+		fi
+	elif [ -f $1 ]; then
+		ln -sf "$(realpath $1)" "$scr_dir/$2"
+		echo "Shortcut created."
 	else
-		if [ -f $1 ]; then
-			ln -sf "$(realpath $1)" "$scr_dir/$2"
-			echo "Shortcut created."
+		if [ "$1" = "-r" ]; then 
+			echo "$2 does not exist."
 		else
 			echo "$1 is not a file."
 		fi
