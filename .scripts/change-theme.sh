67409a2dae40050f91fa8fe25443336511c97f43
Mateus Auler <mateusauler@protonmail.com>
2020-08-26 21:32:51 -0300

    Fixed theme changer with new dmenu build

--- .scripts/change-theme.sh
+++ .scripts/change-theme.sh
@@ -3,7 +3,7 @@
 cd ~/.config/X11/Xresources.d/
 linenum=$(ls *.Xresources | grep -n "^$(tail -n 1 ~/.config/X11/Xresources | sed 's/\#include \"Xresources\.d\/\(.*\)\"/\1/')\$" - | cut -s -d: -f1 | xargs -I {} expr {} - 1)
 [ -z $linenum ] && linenum=0
-newtheme=$(ls *.Xresources | dmenu -wd 900 -g 2 -s $linenum)
+newtheme=$(ls *.Xresources | dmenu -wd 900 -g 2 -ps $linenum)
 [ -z $newtheme ] && exit
 sed -i '$d' ../Xresources
 printf "#include \"Xresources.d/$newtheme\"" >> ../Xresources
