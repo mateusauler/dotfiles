11b31964552b141a66b2fbdee594ab6fadd6f48f
Mateus Auler <mateusauler@protonmail.com>
2020-10-27 21:24:17 -0300

    Update mirrors script has the right permissions

--- .scripts/updmirrors.sh
+++ .scripts/updmirrors.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-sudo reflector -p https,rsync -c "Brazil,Chile,Paraguay,Colombia,Ecuador,United States,Worldwide" -f 24 --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose -p https,rsync -c "Brazil,Chile,Paraguay,Colombia,Ecuador,United States,Worldwide" -f 24 --save /etc/pacman.d/mirrorlist
