e02d374d8ca5d45c2ef474891804cc4f93a19ba4
Mateus Auler <mateusauler@protonmail.com>
2020-11-04 13:02:46 -0300

    Fixed clip.sh argument handling error

--- .scripts/clip.sh
+++ .scripts/clip.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if [ $# -eq 1 ]; then
-	printf $1 | xclip -selection clipboard
+	printf "$1" | xclip -selection clipboard
 elif [ $# -eq 2 ] && [ "$1" = "-f" ]; then
 	xclip -sel clip -t "$(file --mime-type "$2" | cut -d' ' -f2)" "$2"
 fi
