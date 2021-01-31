a46ad32040697c6864ddf1772fafb7d5bc24170a
Mateus Auler <mateusauler@protonmail.com>
2021-01-31 15:31:27 -0300

    config.fish: changed 'p' abbreviation to 'paru'

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -90,7 +90,7 @@ abbr --add ch "c checkout"
 
 abbr --add pa "patch -p1 <"
 
-abbr --add p "sudo pacman -S"
+abbr --add p "paru"
 alias upm "sudo reflector --verbose -p http,https,rsync -c \"Brazil,Chile,Paraguay,Colombia,Ecuador,United States,Worldwide\" -f 24 --completion-percent 100 --save /etc/pacman.d/mirrorlist"
 
 alias hexdump "hexdump -C"
