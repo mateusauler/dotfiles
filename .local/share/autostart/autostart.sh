59b0aeea54edbc102b5b9058a7a00bcc9c4eaf9f
Mateus Auler <mateusauler@protonmail.com>
2021-06-20 18:23:45 -0300

    autostart: Added flameshot

--- .local/share/autostart/autostart.sh
+++ .local/share/autostart/autostart.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-pgrep urxvtd > /dev/null || urxvtd &
 picom --experimental-backends --config $HOME/.config/picom/picom.conf &
 redshift &
-nm-applet &
 megasync &
+flameshot &
+nm-applet &
 unclutter --timeout 3 --ignore-scrolling &
 /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
