0425ae3e2edb82e4f89b1c03f11fddd0fb68d909
Mateus Auler <mateusauler@protonmail.com>
2022-02-18 19:03:11 -0300

    discord.sh: Create discord firejail wrapper

---
+++ .scripts/discord.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+exec firejail --shell=none /bin/discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy $@
