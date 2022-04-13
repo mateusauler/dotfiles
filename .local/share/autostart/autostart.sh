6459961760810578ad4e1e780762dbf7c36a54ba
Mateus Auler <mateusauler@protonmail.com>
2022-04-12 22:41:54 -0300

    autostart: Autostart dunst

--- .local/share/autostart/autostart.sh
+++ .local/share/autostart/autostart.sh
@@ -8,4 +8,5 @@ flameshot &
 nm-applet &
 easyeffects --gapplication-service &
 unclutter --timeout 3 --ignore-scrolling &
+systemctl start --user dunst &
 /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
