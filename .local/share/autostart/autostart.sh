af341d561bf4fddb9e6c8a9939bc5a6da1ed22a4
Mateus Auler <mateusauler@protonmail.com>
2022-04-24 23:31:07 -0300

    autostart: Clipmenud and add its systemd service override

--- .local/share/autostart/autostart.sh
+++ .local/share/autostart/autostart.sh
@@ -9,4 +9,6 @@ nm-applet &
 easyeffects --gapplication-service &
 unclutter --timeout 3 --ignore-scrolling &
 systemctl start --user dunst &
+systemctl --user import-environment DISPLAY &&
+systemctl start --user clipmenud &
 /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
