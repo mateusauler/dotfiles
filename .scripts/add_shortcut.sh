233c613f7969351300e96f9ccca4b05994f29d88
Mateus Auler <mateusauler@protonmail.com>
2022-07-31 22:44:42 -0300

    add_shortcut.sh: Restructure script

--- .scripts/add_shortcut.sh
+++ .scripts/add_shortcut.sh
@@ -1,27 +1,32 @@
 #!/bin/sh
 
 bin_dir=$HOME/.local/bin
+mkdir -p $bin_dir
 
-if [ "$#" = 2 ]; then
-	if [ -e "$bin_dir/$2" ]; then
-		if [ "$1" = "-r" ]; then 
-			rm $bin_dir/$2
-		else
-			echo "Shortcut already exists."
-		fi
-	elif [ -f $1 ]; then
-		ln -sf "$(realpath $1)" "$bin_dir/$2"
-		echo "Shortcut created."
-	else
-		if [ "$1" = "-r" ]; then 
-			echo "$2 does not exist."
-		else
-			echo "$1 is not a file."
-		fi
-	fi
-else
+if [ "$#" != 2 ]; then
 	echo "Usage:"
 	echo "$0 [EXECUTABLE] [SHORTCUT_NAME]"
+	exit 1
 fi
 
+if [ ! -e "$bin_dir/$2" ]; then
+	if [ "$1" = "-r" ]; then
+		echo "$2 does not exist."
+		exit 1
+	fi
 
+	if [ ! -f $1 ]; then
+		echo "$1 is not a file."
+		exit 1
+	fi
+
+	ln -sf "$(realpath $1)" "$bin_dir/$2"
+	exit 0
+fi
+
+if [ "$1" != "-r" ]; then
+	echo "Shortcut already exists."
+	exit 1
+fi
+
+rm $bin_dir/$2
