8c711e752e44512678b0baff98f53dc5848bba0e
Mateus Auler <mateusauler@protonmail.com>
2022-08-08 21:05:35 -0300

    astronvim: Add Flutter extentions to the user config

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,10 +1,18 @@
 return {
-  options = {
-    opt = {
-      tabstop = 4,
-      softtabstop = 4,
-      shiftwidth = 4,
-      expandtab = false
-    }
-  }
+	options = {
+		opt = {
+			tabstop = 4,
+			softtabstop = 4,
+			shiftwidth = 4,
+			expandtab = false
+		},
+		g = {
+			coc_global_extensions = {"coc-flutter"}
+		}
+	},
+	plugins = {
+		init = {
+			{"neoclide/coc.nvim", branch = "release"}
+		}
+	}
 }
