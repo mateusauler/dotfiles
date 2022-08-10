508a4364341042dd28042057b54f7e30e504c46c
Mateus Auler <mateusauler@protonmail.com>
2022-08-10 20:53:42 -0300

    install-astronvim.sh: Refactor and add other dependencies

--- .scripts/install-astronvim.sh
+++ .scripts/install-astronvim.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-paru -S --needed neovim astronvim neovide nerd-fonts-complete xclip
-paru -S --asdeps --needed lazygit python python-neovim nodejs
+cfg_dir=${XDG_CONFIG_HOME:=~/.config}/nvim
 
-if [ -e ~/.config/nvim ] ;
+paru -S --needed neovim astronvim neovide nerd-fonts-complete lazygit npm
+paru -S --asdeps --needed python-neovim
+paru -Rns --noconfirm powerline-vim powerline
+
+if [ -e $cfg_dir/.git ] ;
 then
-	mv ~/.config/nvim ~/.config/nvim-bkp
-fi
+	pushd $cfg_dir > /dev/null
+	git pull
+	popd > /dev/null
+else
+	if [ -e $cfg_dir ] ;
+	then
+		mv $cfg_dir $XDG_CONFIG_HOME/nvim-bkp
+	fi
 
-git clone https://github.com/AstroNvim/AstroNvim ~/.config/nvim
+	git clone https://github.com/AstroNvim/AstroNvim $cfg_dir
+fi
 
 unset VIMINIT
 nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
+sudo nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
