e82da96ba67db54009a5ab27e70a5912a1199fb2
Mateus Auler <mateusauler@protonmail.com>
2022-08-21 23:20:41 -0300

    astronvim: Add rust-tools plugin

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,3 +1,14 @@
+local rt = require("rust-tools")
+
+rt.setup({
+  server = {
+    on_attach = function(_, bufnr)
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover actions" })
+      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr, desc = "Code action groups" })
+    end,
+  },
+})
+
 return {
 	options = {
 		opt = {
@@ -14,7 +25,8 @@ return {
 	plugins = {
 		init = {
 			{"neoclide/coc.nvim", branch = "release"},
-			{"dag/vim-fish", branch = "master"}
+			{"dag/vim-fish", branch = "master"},
+			{"simrat39/rust-tools.nvim", branch = "master"}
 		}
 	}
 }
