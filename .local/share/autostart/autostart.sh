1c794e28ecafd30ff8503c6009158b11736f854b
Mateus Auler <mateusauler@protonmail.com>
2022-10-19 15:32:56 -0300

    autostart: Only run a command if it exists

--- .local/share/autostart/autostart.sh
+++ .local/share/autostart/autostart.sh
@@ -4,15 +4,19 @@ run_file() {
 	[ -f $1 ] && $@
 }
 
-toggle_lang br &
+run_cmd() {
+	[ $(command -v "$1") ] && $@
+}
+
+run_cmd toggle_lang br &
 run_file $HOME/.screenlayout/screenlayout.sh &
-picom --config $HOME/.config/picom/picom.conf &
-redshift &
-megasync &
-flameshot &
-nm-applet &
-easyeffects --gapplication-service &
-unclutter --timeout 3 --ignore-scrolling &
+run_cmd picom --config $HOME/.config/picom/picom.conf &
+run_cmd redshift &
+run_cmd megasync &
+run_cmd flameshot &
+run_cmd nm-applet &
+run_cmd easyeffects --gapplication-service &
+run_cmd unclutter --timeout 3 --ignore-scrolling &
 systemctl start --user dunst &
 systemctl start --user clipmenud &
 run_file /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
