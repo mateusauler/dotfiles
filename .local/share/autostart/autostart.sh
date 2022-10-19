66034e3fa0bcea14bdd3491da5922ba0054fed09
Mateus Auler <mateusauler@protonmail.com>
2022-10-19 20:44:56 -0300

    autostart: Merge run_cmd() & run_file() into run() and run polkit agent

--- .local/share/autostart/autostart.sh
+++ .local/share/autostart/autostart.sh
@@ -1,23 +1,20 @@
 #!/bin/sh
 
-run_file() {
-	[ -f $1 ] && $@
+run() {
+	( [ -f $1 ] || command -v "$1" > /dev/null ) && $@
 }
 
-run_cmd() {
-	[ $(command -v "$1") ] && $@
-}
-
-run_cmd toggle_lang br &
-run_file $HOME/.screenlayout/screenlayout.sh &
-run_cmd picom --config $HOME/.config/picom/picom.conf &
-run_cmd redshift &
-run_cmd megasync &
-run_cmd syncthingtray &
-run_cmd flameshot &
-run_cmd nm-applet &
-run_cmd easyeffects --gapplication-service &
-run_cmd unclutter --timeout 3 --ignore-scrolling &
+run toggle_lang br &
+run $HOME/.screenlayout/screenlayout.sh &
+run picom --config $HOME/.config/picom/picom.conf &
+run redshift &
+run megasync &
+run syncthingtray &
+run flameshot &
+run nm-applet &
+run easyeffects --gapplication-service &
+run unclutter --timeout 3 --ignore-scrolling &
 systemctl start --user dunst &
 systemctl start --user clipmenud &
-run_file /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
+run /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
+run /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
