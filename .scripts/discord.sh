40143528a3c1414cf2ed16b4767e76a01f0077bc
Mateus Auler <mateusauler@protonmail.com>
2023-04-10 23:23:55 -0300

    discord.sh: Remove deprecated firejail argument

--- .scripts/discord.sh
+++ .scripts/discord.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-pgrep Discord &> /dev/null || exec firejail --shell=none /bin/discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy $@
+pgrep Discord &> /dev/null || exec firejail /bin/discord --ignore-gpu-blocklist --disable-features=UseOzonePlatform --enable-features=VaapiVideoDecoder --use-gl=desktop --enable-gpu-rasterization --enable-zero-copy $@
