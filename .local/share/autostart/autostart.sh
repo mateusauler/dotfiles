5b9e5e093daeea7dbe09a8edff41a46bab655c21
Mateus Auler <mateusauler@protonmail.com>
2023-06-14 17:43:09 -0300

    autostart: Add kdeconnect daemon

--- .local/share/autostart/autostart.sh
+++ .local/share/autostart/autostart.sh
@@ -14,3 +14,4 @@ systemctl start --user dunst &
 systemctl start --user clipmenud &
 rsp /etc/X11/xinit/xinitrc.d/50-systemd-user.sh &
 rsp /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 &
+rsp /usr/lib/kdeconnectd &
