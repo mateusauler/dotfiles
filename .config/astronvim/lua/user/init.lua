ebd0ea5f849b858ce6260db9537d2410889a899d
Mateus Auler <mateusauler@protonmail.com>
2023-08-08 20:32:56 -0300

    astronvim: Disable broken configs

--- .config/astronvim/lua/user/init.lua
+++ .config/astronvim/lua/user/init.lua
@@ -1,13 +1,14 @@
-local rt = require("rust-tools")
-
-rt.setup({
-  server = {
-    on_attach = function(_, bufnr)
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover actions" })
-      vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr, desc = "Code action groups" })
-    end,
-  },
-})
+-- local rt = require("rust-tools")
+--
+-- rt.setup({
+-- 	server = {
+-- 		on_attach = function(_, bufnr)
+-- 			vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr, desc = "Hover actions" })
+-- 			vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group,
+-- 				{ buffer = bufnr, desc = "Code action groups" })
+-- 		end,
+-- 	},
+-- })
 
 return {
 	options = {
@@ -16,17 +17,28 @@ return {
 			softtabstop = 4,
 			shiftwidth = 4,
 			expandtab = false,
-			guifont = {"FiraCode Nerd Font Mono", ":h10"}
+			guifont = { "FiraCode Nerd Font Mono", ":h10" }
 		},
 		g = {
-			coc_global_extensions = {"coc-flutter"}
+			coc_global_extensions = { "coc-flutter" }
 		}
 	},
-	plugins = {
-		init = {
-			{"neoclide/coc.nvim", branch = "release"},
-			{"dag/vim-fish", branch = "master"},
-			{"simrat39/rust-tools.nvim", branch = "master"}
-		}
-	}
+	updater = {
+		channel = "stable",
+		version = "latest",
+		remote = "origin",
+		branch = "main",
+		commit = nil,
+		pin_plugins = nil,
+		skip_prompts = false,
+		show_changelog = true,
+		auto_quit = false,
+	},
+	-- plugins = {
+	-- 	init = {
+	-- 		{ "neoclide/coc.nvim",        branch = "release" },
+	-- 		{ "dag/vim-fish",             branch = "master" },
+	-- 		{ "simrat39/rust-tools.nvim", branch = "master" }
+	-- 	}
+	-- }
 }
